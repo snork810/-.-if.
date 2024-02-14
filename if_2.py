95d4df7e7d9ab77e1d5c4770fca2d6425b28e028
Виктор <110487403+snork810@users.noreply.github.com>
2024-02-14 19:38:25 +0300

    Add files via upload

---
+++ if_2.py
@@ -0,0 +1,13 @@
+a, b = 10, 5
+
+if a > b:
+    print('a>b')
+
+if a > b and a > 0:
+    print('успех')
+
+if (a > b) and (a > 0 or b < 1000):
+    print('успех')
+
+if 5 < b and b < 10:
+    print('успех')
